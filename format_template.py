aee651e7ee170f3086c12971fe87017dabfd2836
apryor6@gmail.com <>
2017-09-17 16:16:12 -0400

    test

--- format_template.py
+++ format_template.py
@@ -7,13 +7,19 @@ template_fillers = {
 	'bokeh_glyphs_square':'glyph-square.html',
 	'bokeh_glyphs_triangle':'glyph-triangle.html'
 }
-template_fillers = {k:base_html_filename+v for k, v in template_fillers.items()}
+template_fillers = {k:base_html_filename+v for (k, v) in template_fillers.items()}
 
 format_dict = {}
 for k,v in template_fillers.items():
     with open(v, 'r') as fid:
-    	format_dict.update({k, fid.read()})
+    	format_dict.update({k:fid.read()})
 
-
-with open(template_file,'r') as fi, open(output_filename, 'r') as fo:
-	fo.write(fi.reader().format(format_dict))
+# with open(template_file,'r') as fi, open(output_filename, 'w') as fo:
+	# print(fi.read())
+# for (k,v) in format_dict.items():
+	# print(k, v)
+with open(template_file,'r') as fi, open(output_filename, 'w') as fo:
+	# print(fi.read())
+	# print(fi.read().format(**format_dict))
+	# print(fi.read())
+	fo.write(fi.read().format(**format_dict))
